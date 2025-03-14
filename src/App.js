7991392ce2748483a5c54aaccb71181485a6a8a4
harshtiwari1395 <harshtiwari1395@users.noreply.github.com>
2025-03-14 15:49:13 +0530

    fix: css

--- src/App.js
+++ src/App.js
@@ -142,7 +142,7 @@ const PricingSliderContainer = styled.div`
 
 const Grid = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(320px, 1fr));
   gap: 20px;
   padding: 20px;
 `;
